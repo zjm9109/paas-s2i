9702ef5c4b2fb1d3c86b0b720ddb7517954d2d44
zjm9109 <473870901@qq.com>
2017-08-01 12:04:04 +0800

    Create ApiController.java

---
+++ src/main/java/com/bocloud/paas/s2i/controller/ApiController.java
@@ -0,0 +1,41 @@
+package com.bocloud.paas.s2i.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bocloud.paas.s2i.service.STIServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	@Autowired
+	private STIServiceImpl stiService;
+
+	/**
+	 * 构建s2i镜像
+	 * 
+	 * @param baseImage
+	 *            基础镜像名称
+	 * @param repositoryUrl
+	 *            代码仓库地址
+	 * @param repositoryBranch
+	 *            代码仓库分支，默认master
+	 * @param repositoryUsername
+	 *            代码仓库用户名
+	 * @param repositoryPassword
+	 *            代码仓库密码
+	 * @param warName
+	 *            war包名称
+	 * @param newImage
+	 *            构建后的镜像名称
+	 */
+	@RequestMapping(value = "/build", method = { RequestMethod.GET })
+	public void build(String baseImage, String repositoryUrl, String repositoryBranch, String repositoryUsername,
+			String repositoryPassword, String warName, String newImage) {
+		stiService.build(baseImage, repositoryUrl, repositoryBranch, repositoryUsername, repositoryPassword, warName,
+				newImage);
+	}
+}
