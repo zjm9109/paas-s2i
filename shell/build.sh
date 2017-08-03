ab134f4ba0bf2684b19c2bbe7294bbb13ae5248d
zjm <zjm@bogon>
2017-08-03 10:18:26 +0800

    打印build命令

--- shell/build.sh
+++ shell/build.sh
@@ -1,3 +1,5 @@
 #! /bin/bash  
 
+echo "----------------------------------> s2i build -e PROFILE=docker -e WAR_NAME=$1 -e INCREMENTAL=true --incremental $2 $3 $4 -r $5"
+
 s2i build -e PROFILE=docker -e WAR_NAME=$1 -e INCREMENTAL=true --incremental $2 $3 $4 -r $5
