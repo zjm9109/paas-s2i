063aea040bc601ca07d5b51040e9bf95bdf0a05a
zjm <zjm@bogon>
2017-08-04 10:33:02 +0800

    test

--- src/main/java/com/bocloud/paas/s2i/service/STIServiceImpl.java
+++ src/main/java/com/bocloud/paas/s2i/service/STIServiceImpl.java
@@ -52,8 +52,8 @@ public class STIServiceImpl {
 	public void build(String baseImage, String repositoryUrl, String repositoryBranch, String repositoryUsername,
 			String repositoryPassword, String warName, String newImage) {
 		String shPath = STI_SHELL + BUILD_SH_NAME;
-		String[] command = {"sh", shPath, warName, repositoryUrl, baseImage, newImage, repositoryBranch};
-
+		// String[] command = {"sh", shPath, warName, repositoryUrl, baseImage, newImage, repositoryBranch};
+		String [] command = {"sh", "/opt/docker_build.sh", "build", "-t", "qwer", "."};
 		Result result = ExecuteCommandUtil.exec(command);
 		if (result.getCode() == 0) {
 			logger.info("——————————————————————————————————> execute s2i build success: \n" + result.getMessage());
